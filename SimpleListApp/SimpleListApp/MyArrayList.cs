985234f625bec33268ebcc25015f268ff62c99da
Andrey Proskura <andreyproskura88@gmail.com>
2016-01-29 23:53:28 +0200

    Add

---
+++ SimpleListApp/SimpleListApp/MyArrayList.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleListApp
+{
+    class MyArrayList : IList
+    {
+        int index;
+        double size = 2;
+        double pov = 2;
+        int currentValue;
+
+        object[] myMass;
+
+        public MyArrayList()
+        {
+            currentValue = (int)Math.Pow(size, pov);
+            myMass = new object[currentValue];
+
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return myMass.Length;
+            }
+        }
+
+        public bool IsFixedSize
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsSynchronized
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public object SyncRoot
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Add(object value)
+        {
+            myMass[index++] = value;
+            if (index == currentValue)
+            {
+                currentValue = (int)Math.Pow(size, ++pov);
+                object[] temp = new object[currentValue];
+                for (int i = 0; i < index; i++)
+                {
+                    temp[i] = myMass[i];
+                }
+
+                myMass = temp;
+            }
+
+            return index;
+
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleListApp/SimpleListApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArrayList list = new MyArrayList();
+            list.Add(5);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(41);
+            list.Add(42);
+            list.Add(43);
+            list.Add(44);
+            list.Add(45);
+            list.Add(46);
+            list.Add(47);
+            list.Add(48);
+            list.Add(49);
+            list.Add(40);
+
+            Console.WriteLine(list.Count);
+        }
+    }
+}
